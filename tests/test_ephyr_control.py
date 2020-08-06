d4fc265cbbb8b79c66cbbd25ee49259fe4f12108
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-08-06 23:39:19 +0300

    test: add fixtures for LangStreamControl

--- tests/test_ephyr_control.py
+++ tests/test_ephyr_control.py
@@ -1,5 +1,37 @@
+from unittest.mock import Mock
+
+import pytest
+from zmq import Socket
+
 from ephyr_control import __version__
+from ephyr_control.config import LangStreamConfig, StreamConfig, ZMQConfig
+from ephyr_control.control import StreamCommunicator, LangStreamControl
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@pytest.fixture
+def lang_stream_config():
+    org = StreamConfig(
+        delay="0s", volume=0.5, zmq=ZMQConfig(host="0.0.0.0", port=60010)
+    )
+    trn = StreamConfig(
+        delay="0s", volume=1.0, zmq=ZMQConfig(host="0.0.0.0", port=60020)
+    )
+    return LangStreamConfig(org=org, trn=trn)
+
+
+@pytest.fixture
+def lang_stream_control(lang_stream_config):
+    org = StreamCommunicator.create_org("ru", lang_stream_config)
+    trn = StreamCommunicator.create_trn("ru", lang_stream_config)
+
+    mock_conn = Mock(Socket)
+    mock_conn.recv_string = lambda: "1 Success"
+    org._create_conn = Mock(return_value=mock_conn)
+    trn._create_conn = Mock(return_value=mock_conn)
+    lsc = LangStreamControl("ru", org, trn)
+    lsc.connect()
+    return lsc
